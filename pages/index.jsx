c3abaa8d3e641185b8f20c73051b5124a9da0bf0
Ryosuke <ryosuke.san.hana@gmail.com>
2020-05-12 15:08:44 -0700

    :zap: Example for auth fetch

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,28 @@
+import {useEffect, useState } from 'react'
 import Head from 'next/head'
+import { Heading } from "@chakra-ui/core"
+import Strapi from 'strapi-sdk-javascript'
+import Cookies from 'js-cookie'
 import AuthGuard from '../components/AuthGuard'
 import BaseLayout from '../layouts/BaseLayout'
+import { useUser } from '../context/UserContext'
+import { TOKEN_COOKIES_KEY } from '../constants'
 
 const Dashboard = () => {
+  const [clients, setClients] = useState({})
+  const { user } = useUser()
+  const token = Cookies.get(TOKEN_COOKIES_KEY)
+
+  useEffect(() => {
+    const fetchClients = async () => {
+      const strapi = new Strapi('http://localhost:1337/')
+      strapi.setToken(token)
+      const latestClients = await strapi.getEntries('clients')
+      setClients(latestClients)
+    }
+    fetchClients()
+  }, [token])
+
   return (
     <AuthGuard>
       <BaseLayout>
@@ -12,6 +32,17 @@ const Dashboard = () => {
         </Head>
 
         <div>Private dashboard</div>
+        <div>
+          <Heading as="h2" size="xl">
+            {user.username}
+          </Heading>
+          <Heading as="h4" size="md" fontWeight="normal">
+            {user.email}
+          </Heading>
+        </div>
+        <div>
+          {JSON.stringify(clients)}
+        </div>
       </BaseLayout>
     </AuthGuard>
   )
