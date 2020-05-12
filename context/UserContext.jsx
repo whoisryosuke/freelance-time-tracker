d95bd1e5168725d4d85c6099a8c3e0a8ce7a0932
Ryosuke <ryosuke.san.hana@gmail.com>
2020-05-12 15:06:52 -0700

    :zap: Hydrate user context w API

--- context/UserContext.jsx
+++ context/UserContext.jsx
@@ -1,9 +1,27 @@
-import React, {createContext, useContext, useState} from "react"
+import React, {createContext, useContext, useEffect, useState} from "react"
+import Strapi from 'strapi-sdk-javascript'
+import Cookies from 'js-cookie'
+import { TOKEN_COOKIES_KEY } from '../constants'
 
 export const UserContext = createContext();
 
 export const UserProvider = ({children}) => {
     const [user, updateUser] = useState({})
+    const token = Cookies.get(TOKEN_COOKIES_KEY)
+
+    useEffect(() => {
+      const fetchUser = async () => {
+        const strapi = new Strapi('http://localhost:1337/')
+        strapi.setToken(token)
+
+        const profile = await strapi.getEntries('users/me')
+        if (profile) updateUser(profile)
+
+      }
+      if (Object.keys(user).length === 0) {
+        fetchUser()
+      }
+    }, [user, token])
 
     return (
       <UserContext.Provider value={{ user, updateUser }}>
