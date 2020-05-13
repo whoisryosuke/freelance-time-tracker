4917c3fbfbf4a229b137d30e88a3cb0f7785fc02
Ryosuke <ryosuke.san.hana@gmail.com>
2020-05-13 13:37:00 -0700

    :zap: Add project modal

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -1,21 +1,26 @@
 import { useEffect, useState } from 'react'
 import Head from 'next/head'
 import {
+    Box,
   Button,
   Heading,
+  Stack,
+  Text,
   useDisclosure
 } from '@chakra-ui/core'
 import Strapi from 'strapi-sdk-javascript'
 import Cookies from 'js-cookie'
 import AuthGuard from '../components/AuthGuard'
 import CreateClientModal from '../components/Modals/CreateClientModal'
+import CreateProjectModal from '../components/Modals/CreateProjectModal'
 import BaseLayout from '../layouts/BaseLayout'
 import { TOKEN_COOKIES_KEY } from '../constants'
 
 const Projects = () => {
   const [clients, setClients] = useState({})
   const [projects, setProjects] = useState({})
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { isOpen: clientIsOpen, onOpen: clientOnOpen, onClose: clientOnClose } = useDisclosure()
+  const { isOpen: projectIsOpen, onOpen: projectOnOpen, onClose: projectOnClose } = useDisclosure()
   const token = Cookies.get(TOKEN_COOKIES_KEY)
 
   useEffect(() => {
@@ -38,19 +43,62 @@ const Projects = () => {
           <link rel="icon" href="/favicon.ico" />
         </Head>
 
-        <Heading as="h2" size="xl">
-          Clients
-        </Heading>
-        <div>{JSON.stringify(clients)}</div>
-
-        <Heading as="h2" size="xl">
+        <Heading as="h2" size="xl" mb={3}>
           Projects
         </Heading>
-        <div>{JSON.stringify(projects)}</div>
+        <Stack spacing={3} mb={8}>
+          {projects && projects.length === 0 && (
+            <Text p={5}>No projects found</Text>
+          )}
+          {projects &&
+            projects.length > 0 &&
+            projects.map((project) => (
+              <Box
+                bg={project.Color ? `${project.Color}.500` : 'gray.200'}
+                color="white"
+                p={5}
+                borderRadius={8}
+              >
+                <Heading as="h3" size="md">
+                  {project.Name}
+                </Heading>
+                <Text>{project.Description}</Text>
+              </Box>
+            ))}
+          <Button variantColor="blue" onClick={projectOnOpen}>
+            Create new project
+          </Button>
+        </Stack>
 
-        <Button onClick={onOpen}>Create new client</Button>
+        <Heading as="h2" size="xl" mb={3}>
+          Clients
+        </Heading>
+        <Stack spacing={3} mb={8}>
+          {clients && clients.length === 0 && (
+            <Text p={5}>No clients found</Text>
+          )}
+          {clients &&
+            clients.length > 0 &&
+            clients.map((client) => (
+              <Box
+                bg={client.Color ? `${client.Color}.500` : 'gray.200'}
+                color="white"
+                p={5}
+                borderRadius={8}
+              >
+                <Heading as="h3" size="md">
+                  {client.Name}
+                </Heading>
+                <Text>{client.Description}</Text>
+              </Box>
+            ))}
+          <Button variantColor="blue" onClick={clientOnOpen}>
+            Create new client
+          </Button>
+        </Stack>
 
-        <CreateClientModal isOpen={isOpen} onClose={onClose} />
+        <CreateClientModal isOpen={clientIsOpen} onClose={clientOnClose} />
+        <CreateProjectModal isOpen={projectIsOpen} onClose={projectOnClose} />
       </BaseLayout>
     </AuthGuard>
   )
